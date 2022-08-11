ecfd755a753a30a6149259c78edee5ecabbcfe62
brijr <bridgertower@gmail.com>
2022-08-11 00:02:31 -0600

    2 files modified

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,7 +1,7 @@
-<nav class="flex justify-between items-center">
-    <h2>
+<nav class="sticky top-0 flex justify-between items-center">
+    <a href="/">
         <img class="w-12" src="./logo.svg" alt="Zion Logo">
-    </h2>
+    </a>
 
     <a class="px-4 py-1 hover:pt-2 hover:text-[#C7CEFF] transition-all rounded-lg backdrop-blur-md bg-neutral-800 bg-opacity-75" href="/work">Work</a>
 </nav>
