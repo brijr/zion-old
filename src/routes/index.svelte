d18d2c23f8e38dc0b97cf8358e147615e7cf305e
brijr <bridgertower@gmail.com>
2022-08-11 17:45:58 -0600

    1 file modified

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,27 @@
     <h1>Zion is a <span class="font-serif italic">Design</span> and <span class="font-serif italic">Developement</span> Collective located in Salt Lake City, Utah.</h1>
 </section>
 
+
 <p class="font-light">
     My name is <a class="font-serif italic hover:text-[#C7CEFF] transition-all" href="https://bridger.info">Bridger Tower</a> and I created Zion Design and Development in 2018.  Since it's birth, Zion has designed, developed, and consulted for numerous companies. 
 </p>
+
+<section class="flex gap-4">
+    <a class="w-fit pb-2 pt-12 block flex border-b hover:text-[#C7CEFF] hover:border-[#C7CEFF]" href="https://tally.so/r/31A6rQ">
+    <div class="hover:pr-2 transition-all">Get a Project Quote</div> 
+    <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 15.25V6.75H8.75"></path>
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 7L6.75 17.25"></path>
+    </svg>
+</a>
+<a class="w-fit pb-2 pt-12 block flex border-b hover:text-[#C7CEFF] hover:border-[#C7CEFF]" href="mailto:bridger@ziondesign.io">
+    <div class="hover:pr-2 transition-all">Send us an Email</div> 
+    <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 15.25V6.75H8.75"></path>
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 7L6.75 17.25"></path>
+    </svg>
+</a>
+</section>
 
 <img class="py-12" src="https://images.unsplash.com/photo-1497412159286-98933a0e327f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2335&q=80" alt="mountains and clouds">
 
@@ -32,22 +50,16 @@
     </ul>
 </div>
 
-<section class="flex gap-4">
-    <a class="w-fit pb-2 pt-12 block flex border-b hover:text-[#C7CEFF] hover:border-[#C7CEFF]" href="https://tally.so/r/31A6rQ">
-    <div class="hover:pr-2 transition-all">Get a Project Quote</div> 
-    <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 15.25V6.75H8.75"></path>
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 7L6.75 17.25"></path>
-    </svg>
-</a>
-<a class="w-fit pb-2 pt-12 block flex border-b hover:text-[#C7CEFF] hover:border-[#C7CEFF]" href="mailto:bridger@ziondesign.io">
-    <div class="hover:pr-2 transition-all">Send an Email</div> 
-    <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 15.25V6.75H8.75"></path>
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 7L6.75 17.25"></path>
-    </svg>
-</a>
-</section>
+<h2 class="pt-12">Contact <span class="font-serif italic">Zion</span></h2>
+<iframe
+  src="https://tally.so/embed/wb5KrZ?alignLeft=1&hideTitle=1&transparentBackground=1"
+  width="100%"
+  height="500px"
+  frameborder="0"
+  marginheight="0"
+  marginwidth="0"
+  title="Shoot me a Note">
+</iframe>
 
 <footer class="py-32 text-sm italic font-light">
     © Zion Design LLC, 2018-Present | Created by <a class="font-serif italic hover:text-[#C7CEFF] transition-all" href="https://bridger.info">Bridger Tower</a>
