57a77021642bd52400ab379de2a82ff5d4f41d14
brijr <bridgertower@gmail.com>
2022-08-11 16:47:57 -0600

    1 file modified

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,7 +33,7 @@
 </div>
 
 <section class="flex gap-4">
-    <a class="w-fit pb-2 pt-12 block flex border-b hover:text-[#C7CEFF] hover:border-[#C7CEFF]" href="https://start.ziondesign.io">
+    <a class="w-fit pb-2 pt-12 block flex border-b hover:text-[#C7CEFF] hover:border-[#C7CEFF]" href="https://tally.so/r/31A6rQ">
     <div class="hover:pr-2 transition-all">Get a Project Quote</div> 
     <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 15.25V6.75H8.75"></path>
