c0da6cfc49805699dbd3b60fac5af48ad24dd369
brijr <bridgertower@gmail.com>
2022-08-11 13:23:05 -0600

    1 file modified, 1 added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,60 +6,34 @@
     My name is <a class="font-serif italic hover:text-[#C7CEFF] transition-all" href="https://bridger.info">Bridger Tower</a> and I created Zion Design and Development in 2018.  Since it's birth, Zion has designed, developed, and consulted for numerous companies. 
 </p>
 
-<a class="pt-8 pb-2 block flex border-b hover:text-[#C7CEFF]" href="https://start.ziondesign.io">
-<div class="hover:pr-2 transition-all">Get Started</div> 
-<svg width="24" height="24" fill="none" viewBox="0 0 24 24">
-  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 15.25V6.75H8.75"></path>
-  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 7L6.75 17.25"></path>
-</svg>
+<div class="w-full">
+    <h2 class="pt-8">Zion Serviecs</h2>
 
+    <ul class="list-disc ml-4 font-light">
+        <li>
+            Branding, Logo, and Identity Design
+        </li>
+        <li>
+            Website Design and Developement
+        </li>
+        <li>
+            Search Enging Optimization
+        </li>
+        <li>
+            Graphic and Digital Design
+        </li>
+    </ul>
+</div>
+
+<p class="pt-12 pb-4 font-serif italic">Fill out this form to get started with Zion Design and Development.</p>
+
+<a class="w-fit pb-2 block flex border-b hover:text-[#C7CEFF]" href="https://start.ziondesign.io">
+    <div class="hover:pr-2 transition-all">Get Started</div> 
+    <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 15.25V6.75H8.75"></path>
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 7L6.75 17.25"></path>
+    </svg>
 </a>
-
-<section class="flex flex-wrap gap-2">
-    <div class="w-2/5">
-        <h2 class="pt-8">Zion Serviecs</h2>
-
-        <ul class="list-disc ml-4 font-light">
-            <li>
-                Branding and Identity Design
-            </li>
-            <li>
-                Web Design and Developement
-            </li>
-            <li>
-                Site Content and Copy
-            </li>
-            <li>
-                Search Enging Optimization
-            </li>
-            <li>
-                Branding
-            </li>
-        </ul>
-    </div>
-
-    <div class="w-1/2">
-        <h2 class="pt-8 invisible">Zion Serviecs</h2>
-
-        <ul class="list-disc ml-4 font-light">
-            <li>
-                Branding and Identity Design
-            </li>
-            <li>
-                Web Design and Developement
-            </li>
-            <li>
-                Site Content and Copy
-            </li>
-            <li>
-                Search Enging Optimization
-            </li>
-            <li>
-                Branding
-            </li>
-        </ul>
-    </div>
-</section>
 
 <style>
     h1 {
---
+++ src/routes/work.svelte
@@ -0,0 +1,1 @@
+<h2 class="mt-12">Page build in Progress</h2>
