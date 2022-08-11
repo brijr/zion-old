a60ea261cd39163f33de74bd84a6225ec32de604
brijr <bridgertower@gmail.com>
2022-08-11 00:04:02 -0600

    2 files modified

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,5 +3,5 @@
         <img class="w-12" src="./logo.svg" alt="Zion Logo">
     </a>
 
-    <a class="px-4 py-1 hover:pt-2 hover:text-[#C7CEFF] transition-all rounded-lg backdrop-blur-md bg-neutral-800 bg-opacity-75" href="/work">Work</a>
+    <a class="px-4 py-1 hover:text-[#C7CEFF] hover:font-serif transition-all rounded-lg backdrop-blur-md bg-neutral-800 bg-opacity-75" href="/work">Work</a>
 </nav>
