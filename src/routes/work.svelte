8c7ebb0c76ccddd48fca8e6eb55626d893cb3b9e
brijr <bridgertower@gmail.com>
2022-08-21 13:46:46 -0600

    1 file modified

--- src/routes/work.svelte
+++ src/routes/work.svelte
@@ -8,21 +8,42 @@
     Check out the companies Zion has worked with in the past below. Follow the links to see more of the work.
 </p>
 
-<a class="w-fit pb-2 pt-12 block flex border-b hover:text-[#C7CEFF] hover:border-[#C7CEFF]" href="https://tally.so/r/31A6rQ">
-    <div class="hover:pr-2 transition-all">Get a Project Quote</div> 
-    <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 15.25V6.75H8.75"></path>
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 7L6.75 17.25"></path>
-    </svg>
-</a>
+<section class="flex gap-4">
+    <a class="w-fit pb-2 pt-12 block flex border-b hover:text-[#C7CEFF] hover:border-[#C7CEFF]" href="https://tally.so/r/31A6rQ">
+        <div class="hover:pr-2 transition-all">Get a Project Quote</div> 
+        <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 15.25V6.75H8.75"></path>
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 7L6.75 17.25"></path>
+        </svg>
+    </a>
+    <a class="w-fit pb-2 pt-12 flex block border-b hover:text-[#C7CEFF] hover:border-[#C7CEFF]" href="https://dribbble.com/brijr">
+        <div class="hover:pr-2 transition-all">See my Design Feed</div> 
+        <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 15.25V6.75H8.75"></path>
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 7L6.75 17.25"></path>
+        </svg>
+    </a>
+</section>
+
+
 
 <!-- image -->
 <img class="py-12" src="https://images.unsplash.com/photo-1604515586704-3e8d954b47ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="ocean water in black and white">
 
-<!-- work examples -->
-<section class="my-12 pl-6 border-l border-l-white">
-    <p>We have worked with some <span class="font-serif italic">amazing</span> companies throughout North America.</p>
+<!-- Design Feed -->
+<section class="my-12">
+    <h3 class="font-serif p-0 m-0">Zion Design Feed™</h3>
+
 </section>
+
+<!-- Work Examples -->
+<section class="my-12">
+    <h3 class="font-serif p-0 m-0">Other Recent Work</h3>
+    <a class="hover:pl-6 transition-all block mb-4 pl-4 border-l border-l-white" href="http://hustlecoin.io">Branding and Web Development for HustleCoin</a>
+    <a class="hover:pl-6 transition-all block mb-4 pl-4 border-l border-l-white" href="http://socal.flights">Branding and Web Development for SoCal.Flights</a>
+
+</section>
+
 
 
 <!-- footer -->
@@ -39,5 +60,11 @@
     h2 {
         font-size: 1.5em;
         margin: 1rem 0;
+    }
+
+    h3 {
+        font-size: 1.25em;
+        margin: 1rem 0;
+        font-style: italic;
     }
 </style>
